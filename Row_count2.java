2e4b779e56cf695d53586a5a4345ef414069523b
iqbal8699 <rehandude2214@gmai.com>
2021-11-19 00:13:59 +0530

    just for testing

---
+++ Row_count2.java
@@ -0,0 +1,21 @@
+class Row_count2
+ {
+	public static void main(String[]args)
+	{
+		int sn=1;
+		int en=4;
+		int a=1;
+		int count=5;
+		for (sn=1;sn<=en;sn++)
+		{
+			a=(sn*2);
+			for (int i=1;i<=count;i++)
+			{
+	
+			System.out.print(a);
+		}
+			System.out.println();
+		}
+		}
+	
+}
